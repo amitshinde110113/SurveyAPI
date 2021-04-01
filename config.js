963b1409fbb0225b91c1370a5a6bd19d33caab48
amit-shinde1 <amit.shinde@amdev.in>
2021-04-01 18:08:20 +0530

    added local config if env not found

--- config.js
+++ config.js
@@ -1,9 +1,15 @@
 // config.js
 const dotenv = require('dotenv');
 dotenv.config();
+const localConfig = {
+    host: 'localhost',
+    port: 3000,
+    dbURL: 'DB_URL=mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false',
+    jwtSecret: 'demoSecrect'
+}
 module.exports = {
-    host: process.env.HOST ,
-    port: process.env.PORT,
-    dbURL:process.env.DB_URL,
-    jwtSecret:process.env.JWT_SECRET,
+    host: process.env.HOST || localConfig.host,
+    port: process.env.PORT || localConfig.port,
+    dbURL: process.env.DB_URL || localConfig.dbURL,
+    jwtSecret: process.env.JWT_SECRET || localConfig.jwtSecret,
 };
