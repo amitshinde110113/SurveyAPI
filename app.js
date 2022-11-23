ae8568a81a034f20e43bdb9df7c18e28ad4257e0
Amit Shinde <amit.shinde@techsuperiors.com>
2022-11-23 22:51:32 +0530

    minor change

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
-const express = require('express');
-const logger = require('morgan');
-const masterRouters = require('./api/routes/masterRoutes');
-const { port, host } = require('./config');
-const dbConfig = require('./api/data/dbconfig')
+const express = require("express");
+const logger = require("morgan");
+// const masterRouters = require('./api/routes/masterRoutes');
+const { port, host } = require("./config");
+// const dbConfig = require('./api/data/dbconfig')
 
 const app = express();
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use('/api', masterRouters);
+// app.use("/api", masterRouters);
+
+app.post("/api/post", (req, res, next) => {
+  res.status(200).json({ message: "Post Success" });
+});
+
+app.get("/api/get", (req, res, next) => {
+  res.status(200).json({ message: "Get Success" });
+});
+
+app.put("/api/put", (req, res, next) => {
+  res.status(200).json({ message: "Put Success" });
+});
 
 app.use((req, res, next) => {
-  const error = new Error('Not Found');
-  error['status'] = 404;
+  const error = new Error("Not Found");
+  error["status"] = 404;
   next(error);
 });
 
@@ -23,5 +35,5 @@ app.use((error, req, res, next) => {
 
 app.listen(port, host, () => {
   console.log(`listening on ${host}:${port}`);
-})
+});
 module.exports = app;
