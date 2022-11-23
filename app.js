776b1150c0fa152aa164a3cb5fbae508731df84a
Amit Shinde <amitshinde110113@gmail.com>
2022-11-23 23:48:30 +0530

    minor change

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const logger = require("morgan");
+// const logger = require("morgan");
 // const masterRouters = require('./api/routes/masterRoutes');
 const { port, host } = require("./config");
 // const dbConfig = require('./api/data/dbconfig')
 
 const app = express();
-app.use(logger("dev"));
+// app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // app.use("/api", masterRouters);
